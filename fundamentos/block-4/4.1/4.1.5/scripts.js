76498902838656248fa02a41753d8bbdf985eac4
Matheus Tostes <matheustostes@hotmail.com>
2022-03-07 18:39:13 -0300

    Add exercises 4.1

---
+++ fundamentos/block-4/4.1/4.1.5/scripts.js
@@ -0,0 +1,17 @@
+let state = "lista"
+
+switch (state) {
+    case "aprovada":
+        console.log("Parabéns, você foi aprovada(o)")
+        break
+    case "lista":
+        console.log("Você está na lista de espera")
+        break
+    case "reprovada":
+        console.log("Você foi reprovada(o)")
+        break
+    default:
+        console.log("não se aplica")
+}
+
+
