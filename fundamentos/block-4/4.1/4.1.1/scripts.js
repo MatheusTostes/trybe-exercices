cee9490dbf24e5719a14fefc691b7a1fda25b028
Matheus Tostes <matheustostes@hotmail.com>
2022-03-10 10:18:25 -0300

    Add 4.1 excercises

---
+++ fundamentos/block-4/4.1/4.1.1/scripts.js
@@ -0,0 +1,9 @@
+const myName = "Matheus"
+const birthCity = "Vila Velha"
+var birthYear = 1996
+birthYear = 2030
+birthCity = "Linhares"
+
+console.log(myName)
+console.log(birthCity)
+console.log(birthYear)
